2232ecbbb463f1bd019bfd9d12cef638fd74c21c
andrew <andrew0@live.com.sg>
2023-04-14 14:45:37 +0800

    complete backend

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
+morgan.token('phonebook', (request, response) => {
+  return `"Name": "${request.body.name}", "Number": "${request.body.number}"`;
+})
+
+// handler of requests with result to errors
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// handler of requests with unknown endpoint (unsupported routes)
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-morgan.token('phonebook', (req, res) => {
-  return `"Name": "${req.body.name}", "Number": "${req.body.number}"`;
-});
-
+// handler for json parser should be the first middleware loaded
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms {:phonebook}'));
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Andrew",
-    number: "9183"
-  },
-  {
-   id: 2,
-   name: "Bryan",
-   number: "9184"
-  }
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>My Phone book app</h1>')
-})
-
-app.get('/info', (req, res) => {
-    res.write(`<p>Phonebook has info for ${persons.length} people</p>`)
-    res.write(`${new Date()}`)
+app.get('/info', (request, response) => {
+    response.write(`<p>Phonebook has info for ${Person.length} people</p>`)
+    response.write(`${new Date()}`)
   })
   
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
+  const maxId = Person.length > 0
+    ? Math.max(...Person.map(n => n.id))
     : 0
   return maxId + 1
 }
@@ -66,42 +66,61 @@ app.post('/api/persons', (request, response) => {
         error: 'number missing' 
       })
   }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+    date: new Date()
+  }
+
+  persons.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const person = {
     id: generateId(),
     name: body.name,
     number: body.number,
-    date: new Date(),
+    date: new Date()
   }
 
-  persons = persons.concat(person)
-
-  response.json(person)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(person)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
